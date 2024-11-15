75e6930066b2bd3ea03e2f6e487a1458eacbc83d
rococorbalan <123517270+rococorbalan@users.noreply.github.com>
2024-11-15 19:38:22 -0300

    Add modal with a form to add books
    Now the addBookToLibrar function returns the book
    to be able to call displayBook in one line.

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,46 @@
-const myLibrary = [];
+// Div where the books are stored
 const mainDiv = document.getElementById("books");
 
-function showBooks(library) {
-    for (let book of library) {
-        const bookDiv = document.createElement("div");
+// Dialog
+const dialog = document.querySelector("dialog");
+const addButton = document.getElementById("add");
+const closeButton = document.querySelector("dialog button");
+
+// Dialog Form
+const dialogForm = document.getElementById("add-form");
+const titleInput = document.getElementById("title-input");
+const authorInput = document.getElementById("author-input");
+const pagesInput = document.getElementById("pages-input");
+const readInput = document.getElementById("read-input");
+const saveButton = document.getElementById("save");
+
+
+//Open dialog
+addButton.addEventListener("click", () => {
+    dialog.showModal();
+})
+
+//Close dialog
+closeButton.addEventListener("click", () => {
+    dialog.close();
+})
+
+saveButton.addEventListener("click", (event)=> {
+    event.preventDefault();
+    dialog.close();
+    displayBook(addBookToLibrary(titleInput.value, 
+        authorInput.value, 
+        pagesInput.value, 
+        readInput.checked));
+
+    dialogForm.reset();
+})
+
+const myLibrary = [];
+
+// Show Every Book
+function displayBook(book) {
+    const bookDiv = document.createElement("div");
         for(let property in book){
             const bookProperty = document.createElement("p");
             const bookValue = document.createTextNode(book[property]);
@@ -11,9 +48,9 @@ function showBooks(library) {
             bookDiv.appendChild(bookProperty);
         }
         mainDiv.appendChild(bookDiv);
-    }
 }
 
+// Book Constructor
 function Book(title, author, pages, isRead){
     this.title = title;
     this.author = author;
@@ -21,8 +58,10 @@ function Book(title, author, pages, isRead){
     this.isRead = isRead;
 }
 
-
+// Push book to library array
 function addBookToLibrary(title, author, pages, isRead) {
     book = new Book(title, author, pages, isRead);
     myLibrary.push(book);
+    return book;
 }
+
